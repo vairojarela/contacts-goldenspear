9c8ae26dc51473142a6dca6a9ffca9a0f791d51a
vairojarela <vairojarela@gmail.com>
2019-11-15 22:06:56 +0100

    Updates

---
+++ precache-manifest.16f96eba8a513693bbf296d72b920c51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc0ae22afbb6d442206b56602e474b9",
+    "url": "/vairojarela/contacts-goldenspear/index.html"
+  },
+  {
+    "revision": "5309d7ae80bcf840212e",
+    "url": "/vairojarela/contacts-goldenspear/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "b8a738ce331d23a629b4",
+    "url": "/vairojarela/contacts-goldenspear/static/css/main.e8ef416d.chunk.css"
+  },
+  {
+    "revision": "5309d7ae80bcf840212e",
+    "url": "/vairojarela/contacts-goldenspear/static/js/2.f0ee190d.chunk.js"
+  },
+  {
+    "revision": "570dc1611616f75a682b",
+    "url": "/vairojarela/contacts-goldenspear/static/js/3.a7e375cb.chunk.js"
+  },
+  {
+    "revision": "897211fa7c00b820f7ec",
+    "url": "/vairojarela/contacts-goldenspear/static/js/4.bcae1a72.chunk.js"
+  },
+  {
+    "revision": "b8a738ce331d23a629b4",
+    "url": "/vairojarela/contacts-goldenspear/static/js/main.ddfc09b3.chunk.js"
+  },
+  {
+    "revision": "4523988630c48b1061cc",
+    "url": "/vairojarela/contacts-goldenspear/static/js/runtime-main.3bc961d1.js"
+  }
+]);
