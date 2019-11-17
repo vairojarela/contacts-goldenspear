765320b0816e4f62d68450803d0fceb782773f5e
unknown <vairojarela@gmail.com>
2019-11-17 19:54:13 +0100

    Updates

---
+++ precache-manifest.148fa7321d165419bb10a852e9c48a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56fa623df7b3dc6940420244ca01242",
+    "url": "/contacts-goldenspear/index.html"
+  },
+  {
+    "revision": "08b2eb981971fac9100b",
+    "url": "/contacts-goldenspear/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "2af704ab4f361f05c451",
+    "url": "/contacts-goldenspear/static/css/main.cd83b9a0.chunk.css"
+  },
+  {
+    "revision": "08b2eb981971fac9100b",
+    "url": "/contacts-goldenspear/static/js/2.ea0a471c.chunk.js"
+  },
+  {
+    "revision": "2af704ab4f361f05c451",
+    "url": "/contacts-goldenspear/static/js/main.eb099364.chunk.js"
+  },
+  {
+    "revision": "cecba620e277e2cd0c97",
+    "url": "/contacts-goldenspear/static/js/runtime-main.4c8558c5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contacts-goldenspear/precache-manifest.4be8a936d283397434c9839ce6e57f84.js"
+  "/contacts-goldenspear/precache-manifest.148fa7321d165419bb10a852e9c48a60.js"
 );
 
 self.addEventListener('message', (event) => {
