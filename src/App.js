988353f0386c269de6c3124a193facd88c2bf6dd
unknown <vairojarela@gmail.com>
2019-11-18 16:32:37 +0100

    fix links on deploy

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ import Contacts from "./components/Contacts";
 import ContactDetails from "./components/ContactDetails";
 
 function App() {
-  console.log(process.env.PUBLIC_URL);
   return (
     <Router>
       <Container>
         <Switch>
           <Row>
-            <Route path={"/contacts-goldenspear"} component={Contacts}></Route>
+            <Route path={"/"} component={Contacts}></Route>
 
-            <Route
-              path={"/contacts-goldenspear/:id"}
-              component={ContactDetails}
-            ></Route>
+            <Route path={"/:id"} component={ContactDetails}></Route>
           </Row>
         </Switch>
       </Container>
