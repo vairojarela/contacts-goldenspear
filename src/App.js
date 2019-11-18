220d6f7457a1bf94bf789326e41ecc4d2e8174c9
unknown <vairojarela@gmail.com>
2019-11-18 19:38:07 +0100

    changed to hashrouting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
 import Contacts from "./components/Contacts";
 import ContactDetails from "./components/ContactDetails";
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename="/">
+      >
       <Container>
         <Switch>
           <Row>
             <Route path={"/"} component={Contacts}></Route>
 
-            <Route
-              path={"/client/:id"}
-              exact
-              component={ContactDetails}
-            ></Route>
+            <Route path={"/:id"} exact component={ContactDetails}></Route>
           </Row>
         </Switch>
       </Container>
-    </Router>
+    </HashRouter>
   );
 }
 export default App;
